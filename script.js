7afe54f01d13b96df3fce2de8c5c0f63a52bc4a4
Wafflenugget <trc.for.everyone.in.life@gmail.com>
2024-11-03 18:26:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const serviceID = 'service_hi4zevi';
+const templateID = 'template_nj9xvd7'; 
+const publicKey = 'oLM4xowpzvnN0Y1n1'; 
+
+document.getElementById('yourFormId').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    emailjs.sendForm(service_hi4zevi, template_nj9xvd7, this, oLM4xowpzvnN0Y1n1)
+        .then(function() {
+            alert('Email sent successfully!');
+        }, function(error) {
+            alert('Failed to send email: ' + JSON.stringify(error));
+        });
+});
+
 document.getElementById('poster-section').onclick = function() {
     alert("Send Miss Know It All a letter with your questions!");
 };
