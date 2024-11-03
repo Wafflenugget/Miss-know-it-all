749e5c0fcf4d55be65312911192240d314f68b02
Wafflenugget <trc.for.everyone.in.life@gmail.com>
2024-11-03 16:57:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById('poster-section').onclick = function() {
     alert("Send Miss Know It All a letter with your questions!");
 };
+
+/Miss-Know-It-All-Column
+│
+├── index.html        # Main HTML file (the homepage)
+├── styles.css        # CSS file for styling
+└── script.js         # JavaScript file for interactivity
